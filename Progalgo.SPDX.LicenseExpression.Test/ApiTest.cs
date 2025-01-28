c7cc9fefb647cc6ed7c3d625c94df06e4e5ed744
Roman Ustinov <roman@progalgo.name>
2025-01-28 23:55:47 +0300

    First attempt at interface

---
+++ Progalgo.SPDX.LicenseExpression.Test/ApiTest.cs
@@ -0,0 +1,29 @@
+﻿namespace Progalgo.SPDX.Test;
+
+[TestClass]
+public sealed class ApiTest
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+        LicenseExpression expression = new("MIT OR Apache-2.0");
+    }
+
+    class TestLegalScope : LegalScope<bool>
+    {
+        public override bool Evaluate(License license, IEnumerable<Exception> exceptions)
+        {
+            return true;
+        }
+
+        public override bool EvaluateOption(IEnumerable<bool> vals)
+        {
+            return vals.Any(x => x);
+        }
+
+        public override bool EvaluateAnd(IEnumerable<bool> vals)
+        {
+            return vals.All(x => x);
+        }
+    }
+}
---
+++ Progalgo.SPDX.LicenseExpression/Library.fs
@@ -0,0 +1,15 @@
+﻿namespace Progalgo.SPDX
+
+type LicenseExpression (e: string) = class end
+
+type License (id: string) = class end
+
+type Exception (name: string) = class end
+
+[<AbstractClass>]
+type LegalScope<'value> () =
+    class
+    abstract member Evaluate : License -> Exception seq -> 'value
+    abstract member EvaluateOption : 'value seq -> 'value
+    abstract member EvaluateAnd : 'value seq -> 'value
+    end
