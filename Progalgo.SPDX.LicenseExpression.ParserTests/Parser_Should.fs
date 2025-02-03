c57d837e56cc90c6d9138df103848caf9bea4686
Roman Ustinov <roman@progalgo.name>
2025-02-03 22:28:04 +0300

    Enhance expression parser and add tests

---
+++ Progalgo.SPDX.LicenseExpression.ParserTests/Parser_Should.fs
@@ -0,0 +1,76 @@
+﻿namespace Progalgo.SPDX.LicenseExpression.ParserTests
+
+open Microsoft.VisualStudio.TestTools.UnitTesting
+open Progalgo.SPDX.Parser
+
+[<TestClass>]
+type Parser_Should () =
+
+    let test input =
+        match FParsec.CharParsers.run licenseExpr input with
+        | FParsec.CharParsers.Success(result, _, _) -> Ok result
+        | FParsec.CharParsers.Failure(errorMsg, _, _) -> Error errorMsg
+
+    [<TestMethod>]
+    member this.TestSimpleLicenseId () =
+        let input = "MIT"
+        let expected = (With(LicenseId "MIT", None))
+        match test input with
+            | Ok result -> Assert.AreEqual<LicenseExpression>(expected, result)
+            | Error msg -> Assert.Fail(msg)
+
+    [<TestMethod>]
+    member this.TestLicenseIdWithException () =
+        let input = "MIT WITH Classpath"
+        let expected = With(LicenseId "MIT", Some (Exception "Classpath"))
+        match test input with
+            | Ok result -> Assert.AreEqual<LicenseExpression>(expected, result)
+            | Error msg -> Assert.Fail(msg)
+
+    // [<TestMethod>]
+    // member this.TestLicenseIdWithPlus () =
+    //     let input = "GPL-2.0+"
+    //     let expected = License(LicenseId "GPL-2.0+")
+    //     match test input with
+    //         | Ok result -> Assert.AreEqual<ExpressionTree>(expected, result)
+    //         | Error msg -> Assert.Fail(msg)
+
+    [<TestMethod>]
+    member this.TestLicenseReference () =
+        let input = "LicenseRef-1234"
+        let expected = With(LicenseRef "LicenseRef-1234", None)
+        match test input with
+            | Ok result -> Assert.AreEqual<LicenseExpression>(expected, result)
+            | Error msg -> Assert.Fail(msg)
+
+    [<TestMethod>]
+    member this.TestCompoundExpressionWithAnd () =
+        let input = "MIT AND GPL-2.0"
+        let expected = And(With(LicenseId "MIT", None), With(LicenseId "GPL-2.0", None))
+        match test input with
+            | Ok result -> Assert.AreEqual<LicenseExpression>(expected, result)
+            | Error msg -> Assert.Fail(msg)
+
+    [<TestMethod>]
+    member this.TestCompoundExpressionWithOr () =
+        let input = "MIT OR GPL-2.0"
+        let expected = (Or(With(LicenseId "MIT", None), With(LicenseId "GPL-2.0", None)))
+        match test input with
+            | Ok result -> Assert.AreEqual<LicenseExpression>(expected, result)
+            | Error msg -> Assert.Fail(msg)
+
+    [<TestMethod>]
+    member this.TestNestedCompoundExpression () =
+        let input = "(MIT AND GPL-2.0) OR Apache-2.0"
+        let expected = Or(And(With(LicenseId "MIT", None), With(LicenseId "GPL-2.0", None)), With(LicenseId "Apache-2.0", None))
+        match test input with
+            | Ok result -> Assert.AreEqual<LicenseExpression>(expected, result)
+            | Error msg -> Assert.Fail(msg)
+
+    [<TestMethod>]
+    member this.TestComplexExpressionWithException () =
+        let input = "MIT AND (GPL-2.0 WITH Classpath)"
+        let expected = And(With(LicenseId "MIT", None), With(LicenseId "GPL-2.0", Some (Exception "Classpath")))
+        match test input with
+            | Ok result -> Assert.AreEqual<LicenseExpression>(expected, result)
+            | Error msg -> Assert.Fail(msg)
--- Progalgo.SPDX.LicenseExpression/Parser.fs
+++ Progalgo.SPDX.LicenseExpression/Parser.fs
@@ -1,10 +1,17 @@
 namespace Progalgo.SPDX
 
+open System.Runtime.CompilerServices
+
 module Parser =
+
+    [<assembly: InternalsVisibleTo("Progalgo.SPDX.LicenseExpression.ParserTest")>]
+    do ()
 
     open FParsec
     
 // SPDX License Expression grammar in ABNF
+//
+// Source: https://spdx.github.io/spdx-spec/v2-draft/SPDX-license-expressions/
 //
 // idstring = 1*(ALPHA / DIGIT / "-" / "." )
 // license-id = <short form license identifier in Annex A.1>
@@ -19,32 +26,35 @@ module Parser =
 //   "(" compound-expression ")" )
 // license-expression = (simple-expression / compound-expression)
 
-    type ExpressionTree =
-        | License of string
-        | LicenseWithException of string * string
-        | And of ExpressionTree * ExpressionTree
-        | Or of ExpressionTree * ExpressionTree
+    let idstring = many1Satisfy (fun c -> isLetter c || isDigit c || c = '-' || c = '.')
+    
+    type License =
+        | LicenseId of string
+        | LicenseRef of string
 
-    let parseLicenseId = many1Satisfy (fun c -> isLetter c || isDigit c || c = '-' || c = '.')
-    let parseLicenseRef =
-        opt (pstring "DocumentRef-" >>. parseLicenseId .>> pstring ":") >>. pstring "LicenseRef-" >>. parseLicenseId
-    let parseSimpleExpression = 
-        (parseLicenseId .>> pstring "+" |>> License)
-        <|> (parseLicenseId |>> License)
-        <|> (parseLicenseRef |>> License)
-    let parseLicenseWithException =
-        parseLicenseId .>> pstring "WITH" .>>. parseLicenseId |>> LicenseWithException
-    let parseCompoundExpressionRef = ref (fun _ -> failwith "uninitialized")
-    let rec parseCompoundExpression = parseCompoundExpressionRef.Value
-    do parseCompoundExpressionRef := 
-        (parseSimpleExpression .>> pstring "AND" .>>. parseCompoundExpression |>> And)
-        <|> (parseSimpleExpression .>> pstring "OR" .>>. parseCompoundExpression |>> Or)
-        <|> (parseSimpleExpression .>>. parseCompoundExpression |>> And)
-        <|> (parseSimpleExpression |>> id)
-        <|> (between (pstring "(") (pstring ")") parseCompoundExpression)
-    let parseLicenseExpression = parseCompoundExpression <|> parseSimpleExpression
+    type Exception = Exception of string
 
-    let runParser input =
-        match run parseLicenseExpression input with
-        | Success(result, _, _) -> Some(result)
-        | Failure(_, _, _) -> None
+    type LicenseExpression =
+        | With of License * Exception Option
+        | And of LicenseExpression * LicenseExpression
+        | Or of LicenseExpression * LicenseExpression
+
+    let licenseRef =
+        opt (pstring "DocumentRef-" >>. idstring .>> pstring ":") .>>. (pstring "LicenseRef-" >>. idstring) |>> fun (d, l) -> LicenseRef (sprintf "%sLicenseRef-%s" (defaultArg d "") l)
+    
+    let license = choice [
+        licenseRef
+        idstring |>> LicenseId
+    ]
+    
+    let licenseWithException =
+        license .>>. opt (pstring " WITH " >>. idstring |>> Exception) |>> With
+    
+    let opp = new OperatorPrecedenceParser<LicenseExpression, unit, unit>()
+
+    opp.TermParser <- licenseWithException <|> between (pstring "(") (pstring ")") opp.ExpressionParser
+
+    opp.AddOperator(InfixOperator(" AND", spaces1, 1, Associativity.Left, fun x y -> And(x, y)))
+    opp.AddOperator(InfixOperator(" OR", spaces1, 2, Associativity.Left, fun x y -> Or(x, y)))
+
+    let licenseExpr = opp.ExpressionParser
